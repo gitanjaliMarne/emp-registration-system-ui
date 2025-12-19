ac44836717f1fff9564d1153ede4c665c318239b
gitanjaliMarne <gitanjali4221@gmail.com>
2025-12-19 17:11:35 +0530

    Formatting

--- src/app/auth/signup/signup.ts
+++ src/app/auth/signup/signup.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Auth } from '../../services/auth';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -15,15 +15,29 @@ export class Signup {
   signupForm;
 
   constructor(
-    private fb: FormBuilder,
-    private auth: Auth,
-    private router: Router
+    private readonly fb: FormBuilder,
+    private readonly auth: Auth,
+    private readonly router: Router
   ) {
     this.signupForm = this.fb.group({
       fullName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    });
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required]]
+    }, { validators: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator(form: AbstractControl) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+
+    if (password !== confirmPassword) {
+      form.get('confirmPassword')?.setErrors({ passwordMismatch: true });
+    } else {
+      form.get('confirmPassword')?.setErrors(null);
+    }
+
+    return null;
   }
 
   submit() {
@@ -32,7 +46,8 @@ export class Signup {
     const signupForm = {
       email: this.signupForm.get('email')?.value,
       fullName: this.signupForm.get('fullName')?.value,
-      password: this.signupForm.get('password')?.value
+      password: this.signupForm.get('password')?.value,
+      confirmPassword: this.signupForm.get('confirmPassword')?.value
     };
 
     this.auth.signup(signupForm).subscribe({
--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,20 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
+import { api } from '../utils/constant';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
-  private api = 'http://localhost:8080/api/auth';
-
-  constructor(private http: HttpClient) {}
+  constructor(private readonly http: HttpClient) {}
 
   signup(signupForm : any): Observable<any> {
-    return this.http.post(`${this.api}/signup`, signupForm);
+    return this.http.post(`${api}/auth/signup`, signupForm);
   }
 
   login(loginForm: any): Observable<any> {
-    return this.http.post(`${this.api}/login`, loginForm);
+    return this.http.post(`${api}/auth/login`, loginForm);
   }
 }
